eba50ebe648ebf809e0305915ab2f581e592e638
BaderEddineOuaich <badereddineouaich@gmail.com>
2020-11-20 22:46:41 +0100

    Configured Linux (gmake2) & MacOS(xcode) project generating scripts

--- Gen-Linux-GMake2.sh
+++ Gen-Linux-GMake2.sh
@@ -1,3 +1,2 @@
-# Generates GMake2 project using premake5.exe and premake5.lua script #
-cd Premake/Linux
-premake5 gmake2
+# Generates GMake2 project using premake5 and premake5.lua script #
+./Premake/Linux/premake5 gmake2
--- Gen-MacOS-XCode.sh
+++ Gen-MacOS-XCode.sh
@@ -1,3 +1,2 @@
 # Generates XCode project using premake5.exe and premake5.lua script #
-cd Premake/MacOS
-premake5 xcode4
+./Premake/MacOS/premake5 xcode4
