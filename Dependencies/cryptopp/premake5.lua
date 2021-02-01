c27b6da110e6f38549aace8c0d59767177431486
BaderEddineOuaich <badereddineouaich@gmail.com>
2021-02-01 21:24:42 +0100

    Updated cryptopp premake5.lua

--- Dependencies/cryptopp/premake5.lua
+++ Dependencies/cryptopp/premake5.lua
@@ -17,7 +17,7 @@ project "cryptopp"
 		NEON, Aarch32, Aarch64, Power4, Power7 or Power8.
 		#define CRYPTOPP_DISABLE_ASM 1
 		]] 
-		"CRYPTOPP_DISABLE_ASM=1",
+		--"CRYPTOPP_DISABLE_ASM=1",
 
 		--"CRYPTOPP_ENABLE_NAMESPACE_WEAK", -- enable only when using MD5 to disable warning 'You may be using a weak algorithm that has been retained for backwards compatibility...'
 		--"CRYPTOPP_IMPORTS", -- for static build
@@ -25,7 +25,7 @@ project "cryptopp"
 	}
 
 	-- Enable SSE2 vector processing https://github.com/premake/premake-core/wiki/vectorextensions
-	--vectorextensions "SSE2"
+	vectorextensions "SSE2"
 
 
 	files
@@ -103,6 +103,7 @@ project "cryptopp"
 		systemversion "latest"
 		files
 		{
+			"x64masm.asm", "x64dll.asm",
 		}
 		defines
 		{
