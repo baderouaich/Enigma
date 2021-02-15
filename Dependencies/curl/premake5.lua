ba903054f6939b642cb94c662f100d2f6377a108
BaderEddineOuaich <badereddineouaich@gmail.com>
2021-02-16 00:33:30 +0100

    added ca bundle file locations for cURL in linux based systems

--- Dependencies/curl/premake5.lua
+++ Dependencies/curl/premake5.lua
@@ -42,7 +42,8 @@ project "curl"
 	 		"Crypt32"
 	    }
  
-	filter "system:linux"
+ 
+	filter "system:linux or bsd or solaris"
 		defines
 		{
 			"CURL_HIDDEN_SYMBOLS"
@@ -50,6 +51,25 @@ project "curl"
 		links
 		{
 		}
+		-- find the location of the ca bundle
+		local ca = nil
+		for _, f in ipairs {
+			"/etc/ssl/certs/ca-certificates.crt",
+			"/etc/pki/tls/certs/ca-bundle.crt",
+			"/usr/share/ssl/certs/ca-bundle.crt",
+			"/usr/local/share/certs/ca-root.crt",
+			"/usr/local/share/certs/ca-root-nss.crt",
+			"/etc/certs/ca-certificates.crt",
+			"/etc/ssl/cert.pem" } do
+			if os.isfile(f) then
+				ca = f
+				break
+			end
+		end
+		if ca then
+			defines { 'CURL_CA_BUNDLE="' .. ca .. '"' }
+		end
+
 
 	filter "system:macosx"
 		defines
