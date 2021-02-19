708288e4d49adff7d4325a98e0090f7a46421856
BaderEddineOuaich <badereddineouaich@gmail.com>
2021-02-19 21:27:42 +0100

    fixed curl build issue on linux

--- premake5.lua
+++ premake5.lua
@@ -166,8 +166,8 @@ project "Enigma"
 		links
 		{
 			-- static libraries do not include their dependencies => https://stackoverflow.com/questions/58118344/transitive-library-dependency-in-premake
-			--"zlib",
-			--"mbedtls",
+			"zlib",
+			"mbedtls",
 
 			"X11", -- x11 Linux gui libs
 			"dl", -- dynamic loader interface
