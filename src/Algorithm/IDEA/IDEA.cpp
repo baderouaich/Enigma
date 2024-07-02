50f8455bc572079df828f3e5c55b44702196a945
Bader <badereddineouaich@gmail.com>
2024-07-02 17:46:20 +0100

    Handled large file support in IDEA algorithm

--- src/Algorithm/IDEA/IDEA.cpp
+++ src/Algorithm/IDEA/IDEA.cpp
@@ -1,109 +1,235 @@
-#include "pch.hpp"
 #include "IDEA.hpp"
+#include <pch.hpp>
 
 NS_ENIGMA_BEGIN
 
 IDEA::IDEA(const Algorithm::Intent intent) noexcept
-	:
-	Algorithm(Algorithm::Type::IDEA, intent)
-{
+    : Algorithm(Algorithm::Type::IDEA, intent) {
   if (static_cast<bool>(intent & Intent::Encrypt))
-		m_idea_encryptor = std::make_unique<CryptoPP::EAX<CryptoPP::IDEA>::Encryption>();
+    m_idea_encryptor = std::make_unique<CryptoPP::EAX<CryptoPP::IDEA>::Encryption>();
   if (static_cast<bool>(intent & Intent::Decrypt))
-		m_idea_decryptor = std::make_unique<CryptoPP::EAX<CryptoPP::IDEA>::Decryption>();
+    m_idea_decryptor = std::make_unique<CryptoPP::EAX<CryptoPP::IDEA>::Decryption>();
 }
 
-IDEA::~IDEA() noexcept
-{
+IDEA::~IDEA() noexcept {
 }
 
-std::string IDEA::Encrypt(const std::string& password, const std::string& buffer)
-{
-	// Make sure encryption mode and the seeder are initialized & Validate Arguments
-	{
-		ENIGMA_ASSERT_OR_THROW(m_idea_encryptor, "IDEA Encryptor is not initialized properly");
-		ENIGMA_ASSERT_OR_THROW(m_auto_seeded_random_pool, "IDEA Encryptor seeder is not initialized properly");
-		// Password length must be at least 9 for security reasons
-		ENIGMA_ASSERT_OR_THROW(password.size() >= Constants::Algorithm::MINIMUM_PASSWORD_LENGTH, "IDEA Minimum Password Length is " + std::to_string(Constants::Algorithm::MINIMUM_PASSWORD_LENGTH));
-		//No max password check since we using KDF SHA-256, his allows you to use a password smaller or larger than the cipher's key size: https://crypto.stackexchange.com/questions/68299/length-of-password-requirement-using-openssl-aes-256-cbc
-	}
+std::vector<byte> IDEA::Encrypt(const std::string& password, const std::vector<byte>& buffer) {
+  ENIGMA_ASSERT_OR_THROW(m_idea_encryptor, "IDEA Encryptor is not initialized properly");
+  ENIGMA_ASSERT_OR_THROW(password.size() >= Constants::Algorithm::MINIMUM_PASSWORD_LENGTH, "For security reasons, IDEA minimum password length is " + std::to_string(Constants::Algorithm::MINIMUM_PASSWORD_LENGTH));
 
+  EnigmaFooter footer{};
+  footer.magic = ENIGMA_MAGIC;
+  footer.version = ENIGMA_VERSION_MAJOR * 100'000 + ENIGMA_VERSION_MINOR * 1000 + ENIGMA_VERSION_PATCH;
+  footer.algo = static_cast<byte>(this->GetType());
+  footer.iv = Algorithm::GenerateRandomIV(CryptoPP::IDEA::BLOCKSIZE);
+  footer.hash = HashUtils::bytes<CryptoPP::SHA256>(buffer);
+  footer.extra = {};
 
-	// Randomly generated IV
-	const std::string iv = Algorithm::GenerateRandomIV(CryptoPP::IDEA::BLOCKSIZE);
+  // Encryption key to be generated from user password + IV
+  CryptoPP::SecByteBlock key(static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH) + static_cast<std::size_t>(CryptoPP::IDEA::BLOCKSIZE));
+  // Convert key to KDF SHA-256, which allows you to use a password smaller or larger than the cipher's key size
+  CryptoPP::HKDF<CryptoPP::SHA256> hkdf{};
+  hkdf.DeriveKey(
+    key, key.size(),
+    reinterpret_cast<const byte *>(password.data()), password.size(),
+    reinterpret_cast<const byte *>(footer.iv.data()), footer.iv.size(),
+    nullptr, 0);
+  // Set Key and IV to the encryptor
+  m_idea_encryptor->SetKeyWithIV(key, static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH), key + static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH)); // key, kl, iv, ivl
 
-	// Prepare key
-	CryptoPP::SecByteBlock key(static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH) + static_cast<std::size_t>(CryptoPP::IDEA::BLOCKSIZE)); // Encryption key to be generated from user password + IV
+  // Encrypt
+  std::vector<byte> cipher;
+  const CryptoPP::VectorSource vs(
+    buffer,
+    true,
+    new CryptoPP::AuthenticatedEncryptionFilter( // note: for GCM mode, use AuthenticatedEncryptionFilter instead of StreamTransformationFilter
+      *m_idea_encryptor,
+      new CryptoPP::VectorSink(cipher))); //NOTE: VectorSource will auto clean the allocated memory
 
-	// Convert key to KDF SHA-256, which allows you to use a password smaller or larger than the cipher's key size
-	CryptoPP::HKDF<CryptoPP::SHA256> hkdf{};
-	hkdf.DeriveKey(
-		key, key.size(),
-		reinterpret_cast<const byte*>(password.data()), password.size(),
-		reinterpret_cast<const byte*>(iv.data()), iv.size(),
-		nullptr, 0
-	);
-
-	// Set Key and IV to the encryptor
-	m_idea_encryptor->SetKeyWithIV(key, CryptoPP::IDEA::MAX_KEYLENGTH, key + CryptoPP::IDEA::MAX_KEYLENGTH); // key, kl, iv, ivl
-
-	// Encrypt
-	std::string cipher{}; // Final encrypted buffer
-	[[maybe_unused]] const auto ss = CryptoPP::StringSource(
-		buffer,
-		true,
-		//new CryptoPP::StreamTransformationFilter(
-		new CryptoPP::AuthenticatedEncryptionFilter(
-			*m_idea_encryptor,
-			new CryptoPP::StringSink(cipher)
-		)); //NOTE: StringSource will auto clean the allocated memory
-	
-
-	// Output will be (Algorithm Type + IV + Cipher) since we need IV and Algorithm used for encryption later for decryption
-	std::ostringstream output{};
-	output
-		<< static_cast<char>(this->GetType()) // Append Algorithm Type (enum id)
-		<< iv // Append IV
-		<< cipher; // Append Cipher
-	return output.str();
+  std::vector<byte> out;
+  // Append Cipher
+  out.insert(out.end(), cipher.begin(), cipher.end());
+  // Append EnigmaFooter
+  std::vector<byte> footerBytes = footer.toBytes();
+  out.insert(out.end(), footerBytes.begin(), footerBytes.end());
+  return out;
 }
 
-std::string IDEA::Decrypt(const std::string& password, const std::string& algotype_iv_cipher)
-{
-	// Make sure decryption mode is initialized
-	ENIGMA_ASSERT_OR_THROW(m_idea_decryptor, "IDEA Decryptor is not initialized properly");
+std::vector<byte> IDEA::Decrypt(const std::string& password, const std::vector<byte>& cipher) {
+  ENIGMA_ASSERT_OR_THROW(isEnigmaCipher(cipher), "Given cipher is malformed or was not encrypted with Enigma");
+  ENIGMA_ASSERT_OR_THROW(m_idea_decryptor, "IDEA Decryptor is not initialized properly");
 
-	// Extract IV and Cipher from algotype_iv_cipher (we output cipher as AlgoType + IV + Cipher)
-	const std::string iv = algotype_iv_cipher.substr(sizeof(Algorithm::Type), CryptoPP::IDEA::BLOCKSIZE);
-	ENIGMA_ASSERT_OR_THROW(!iv.empty(), "Failed to extract IV part from algotype_iv_cipher");
-	const std::string cipher = algotype_iv_cipher.substr(sizeof(Algorithm::Type) + CryptoPP::IDEA::BLOCKSIZE, algotype_iv_cipher.size() - 1);
-	ENIGMA_ASSERT_OR_THROW(!cipher.empty(), "Failed to extract cipher part from algotype_iv_cipher");
+  // Extract footer
+  EnigmaFooter footer = EnigmaFooter::fromBytes(cipher);
+  // Prepare Key
+  CryptoPP::SecByteBlock key(static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH) + static_cast<std::size_t>(CryptoPP::IDEA::BLOCKSIZE));
+  // Convert key to KDF SHA-256, which allows you to use a password smaller or larger than the cipher's key size
+  CryptoPP::HKDF<CryptoPP::SHA256> hkdf;
+  hkdf.DeriveKey(
+    key, key.size(),
+    reinterpret_cast<const byte *>(password.data()), password.size(),
+    reinterpret_cast<const byte *>(footer.iv.data()), footer.iv.size(),
+    nullptr, 0);
 
-	// Prepare Key
-	CryptoPP::SecByteBlock key(CryptoPP::IDEA::MAX_KEYLENGTH + CryptoPP::IDEA::BLOCKSIZE);
+  // Set Key and IV to the decrypter
+  m_idea_decryptor->SetKeyWithIV(key, static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH), key + static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH)); // key, kl, iv, ivl
 
-	// Convert key to KDF SHA-256, which allows you to use a password smaller or larger than the cipher's key size
-	CryptoPP::HKDF<CryptoPP::SHA256> hkdf{};
-	hkdf.DeriveKey(
-		key, key.size(),
-		reinterpret_cast<const byte*>(password.data()), password.size(),
-		reinterpret_cast<const byte*>(iv.data()), iv.size(),
-		nullptr, 0);
+  // Decrypt
+  std::vector<byte> decrypted;
+  [[maybe_unused]] const auto ss = CryptoPP::ArraySource(
+    cipher.data(),
+    cipher.size() - footer.sizeInBytes(),
+    true,
+    new CryptoPP::AuthenticatedDecryptionFilter(
+      *m_idea_decryptor,
+      new CryptoPP::VectorSink(decrypted)));
 
-	// Set Key and IV to the decryptor
-	m_idea_decryptor->SetKeyWithIV(key, CryptoPP::IDEA::MAX_KEYLENGTH, key + CryptoPP::IDEA::MAX_KEYLENGTH); // key, kl, iv, ivl
+  // Ensure decryption is successful
+  ENIGMA_ASSERT_OR_THROW(HashUtils::bytes<CryptoPP::SHA256>(decrypted) == footer.hash, "Decryption failure. Original SHA256 hash of buffer does not match decrypted hash");
+  return decrypted;
+}
 
-	// Decrypt
-	std::string decrypted{}; // Recovered buffer
-	const CryptoPP::StringSource ss(
-		cipher,
-		true,
-		new CryptoPP::AuthenticatedDecryptionFilter(
-			*m_idea_decryptor,
-			new CryptoPP::StringSink(decrypted)
-		)); //NOTE: StringSource will auto clean the allocated memory
 
-	return decrypted;
+void IDEA::Encrypt(const std::string& password, const fs::path& in_filename, const fs::path& out_filename) {
+  ENIGMA_ASSERT_OR_THROW(!isEnigmaFile(in_filename), in_filename.filename().string() + " is already encrypted with Enigma");
+  ENIGMA_ASSERT_OR_THROW(password.size() >= Constants::Algorithm::MINIMUM_PASSWORD_LENGTH, "IDEA Minimum Password Length is " + std::to_string(Constants::Algorithm::MINIMUM_PASSWORD_LENGTH));
+  ENIGMA_ASSERT_OR_THROW(m_idea_encryptor, "IDEA Encryptor is not initialized properly");
+
+  EnigmaFooter footer{};
+  footer.magic = ENIGMA_MAGIC;
+  footer.version = ENIGMA_VERSION_MAJOR * 100'000 + ENIGMA_VERSION_MINOR * 1000 + ENIGMA_VERSION_PATCH;
+  footer.algo = static_cast<byte>(this->GetType());
+  footer.iv = Algorithm::GenerateRandomIV(CryptoPP::IDEA::BLOCKSIZE);
+  footer.extra = {};
+
+  // Encryption key to be generated from user password + IV
+  CryptoPP::SecByteBlock key(static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH) + static_cast<std::size_t>(CryptoPP::IDEA::BLOCKSIZE));
+  // Convert key to KDF SHA-256, which allows you to use a password smaller or larger than the cipher's key size
+  CryptoPP::HKDF<CryptoPP::SHA256> hkdf{};
+  hkdf.DeriveKey(
+    key, key.size(),
+    reinterpret_cast<const byte *>(password.data()), password.size(),
+    reinterpret_cast<const byte *>(footer.iv.data()), footer.iv.size(),
+    nullptr, 0);
+
+  // Now encrypt file chunk by chunk
+  std::ofstream ofs{out_filename, std::ios::binary};
+  CryptoPP::SHA256 sha256{};
+  bool ok = ofs.good();
+  FileUtils::ReadChunks(in_filename, ENIGMA_BUFFER_DEFAULT_SIZE, [this, &ok, &ofs, &key, &sha256](std::vector<byte>&& chunk) -> bool {
+    try {
+      // Encrypt chunk
+      // Set Key and IV to the encryptor
+      m_idea_encryptor->SetKeyWithIV(key, static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH), key + static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH)); // key, kl, iv, ivl
+
+      EnigmaCipherChunk cipherChunk{};
+      cipherChunk.magic = ENIGMA_CIPHER_CHUNK_MAGIC;
+      const CryptoPP::VectorSource vs(
+        chunk,
+        true,
+        new CryptoPP::AuthenticatedEncryptionFilter( // note: for GCM mode, use AuthenticatedEncryptionFilter instead of StreamTransformationFilter
+          *m_idea_encryptor,
+          new CryptoPP::VectorSink(cipherChunk.cipher))); //NOTE: VectorSource will auto clean the allocated memory
+      // Save chunk bytes to ofs
+      const std::vector<byte> cipherChunkBytes = cipherChunk.toBytes();
+      ok &= ofs.write(reinterpret_cast<const char *>(cipherChunkBytes.data()), cipherChunkBytes.size()).good();
+      // Also profit from the occasion to calculate sha256 hash
+      sha256.Update(chunk.data(), chunk.size());
+      return ok;
+    } catch (const CryptoPP::Exception& e) {
+      ok &= false;
+      ENIGMA_ERROR("Could not encrypt chunk: {}", e.what());
+    } catch (const std::exception& e) {
+      ok &= false;
+      ENIGMA_ERROR("Could not encrypt chunk: {}", e.what());
+    } catch (...) {
+      ok &= false;
+      ENIGMA_ERROR("Could not encrypt chunk: UNKNOWN ERROR");
+    }
+    return ok;
+  });
+
+  // No need to keep out_filename on failed encryption
+  if (!ok) {
+    ofs.close();
+    fs::remove(out_filename);
+    throw std::runtime_error("Could not encrypt file " + in_filename.string());
+  }
+
+  // Alles gut!
+  // Append enigma footer info
+  ofs.seekp(0, std::ios::end);
+  sha256.Final(footer.hash.data());
+  std::vector<byte> footerBytes = footer.toBytes();
+  ofs.write(reinterpret_cast<const char *>(footerBytes.data()), footerBytes.size());
+  ENIGMA_ASSERT_OR_THROW(ofs.good(), "Failed to write footer bytes");
+  ofs.close();
+}
+
+void IDEA::Decrypt(const std::string& password, const fs::path& in_filename, const fs::path& out_filename) {
+  ENIGMA_ASSERT_OR_THROW(isEnigmaFile(in_filename), "Given file " + in_filename.string() + " is malformed or was not encrypted with Enigma");
+  ENIGMA_ASSERT_OR_THROW(m_idea_decryptor, "IDEA Decryptor is not initialized properly");
+
+  // Extract footer from encrypted file
+  EnigmaFooter footer = EnigmaFooter::fromFile(in_filename);
+  // Prepare Key
+  CryptoPP::SecByteBlock key(static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH) + static_cast<std::size_t>(CryptoPP::IDEA::BLOCKSIZE));
+  // Convert key to KDF SHA-256, which allows you to use a password smaller or larger than the cipher's key size
+  CryptoPP::HKDF<CryptoPP::SHA256> hkdf;
+  hkdf.DeriveKey(
+    key, key.size(),
+    reinterpret_cast<const byte *>(password.data()), password.size(),
+    reinterpret_cast<const byte *>(footer.iv.data()), footer.iv.size(),
+    nullptr, 0);
+
+  // Decrypt file chunk by chunk
+  std::ofstream ofs{out_filename, std::ios::binary};
+  CryptoPP::SHA256 sha256;
+  bool ok = ofs.good();
+  readCipherChunks(in_filename, [this, &ok, &ofs, &key, &sha256](EnigmaCipherChunk&& cipherChunk) -> bool {
+    try {
+      // Decrypt chunk
+      // Set Key and IV to the decrypter
+      m_idea_decryptor->SetKeyWithIV(key, static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH), key + static_cast<std::size_t>(CryptoPP::IDEA::MAX_KEYLENGTH)); // key, kl, iv, ivl
+
+      std::vector<byte> decrypted{};
+      [[maybe_unused]] const auto ss = CryptoPP::VectorSource(
+        cipherChunk.cipher,
+        true,
+        new CryptoPP::AuthenticatedDecryptionFilter(
+          *m_idea_decryptor,
+          new CryptoPP::VectorSink(decrypted)));
+      // Write decrypted chunk to ofs
+      ok &= ofs.write(reinterpret_cast<const char *>(decrypted.data()), decrypted.size()).good();
+      // Also profit from the occasion to calculate sha256 hash
+      sha256.Update(decrypted.data(), decrypted.size());
+      return ok;
+    } catch (const CryptoPP::Exception& e) {
+      ok &= false;
+      ENIGMA_ERROR("Could not decrypt chunk: {}", e.what());
+    } catch (const std::exception& e) {
+      ok &= false;
+      ENIGMA_ERROR("Could not decrypt chunk: {}", e.what());
+    } catch (...) {
+      ok &= false;
+      ENIGMA_ERROR("Could not decrypt chunk: UNKNOWN ERROR");
+    }
+    return ok;
+  });
+  // No need to keep out_filename on failed decryption
+  if (!ok) {
+    ofs.close();
+    fs::remove(out_filename);
+    throw std::runtime_error("Could not decrypt file " + in_filename.string());
+  }
+  ofs.close();
+
+  // Ensure decryption is successful
+  std::array<byte, CryptoPP::SHA256::DIGESTSIZE> digest{};
+  sha256.Final(digest.data());
+  ENIGMA_INFO("Verifying SHA256 hash of {} ...", out_filename.filename().string());
+  ENIGMA_ASSERT_OR_THROW(digest == footer.hash, "Decryption failure. Original SHA256 hash of file does not match decrypted hash");
 }
 
 NS_ENIGMA_END
--- src/Algorithm/IDEA/IDEA.hpp
+++ src/Algorithm/IDEA/IDEA.hpp
@@ -1,7 +1,4 @@
 #pragma once
-#ifndef ENIGMA_IDEA_H
-#define ENIGMA_IDEA_H
-
 #include <Algorithm/Algorithm.hpp>
 
 NS_ENIGMA_BEGIN
@@ -11,25 +8,23 @@ NS_ENIGMA_BEGIN
 *	IDEA is the International Data Encryption Standard by Massey and Lai. The cipher is a 64-bit block cipher which uses 126-bit
 *	keys and a 64-bit initialization vector (IV). IDEA is a popular choice in open source projects such as OpenPGP.
 */
-class IDEA : public Algorithm
-{
-public:
-	/**
-	*	@param intent: Operation, Encrypt or Decrypt
-	*/
-	explicit IDEA(const Algorithm::Intent intent) noexcept;
-	virtual ~IDEA() noexcept;
+class IDEA : public Algorithm {
+  public:
+    /**
+    *	@param intent: Operation, Encrypt or Decrypt
+    */
+    explicit IDEA(const Algorithm::Intent intent) noexcept;
+    virtual ~IDEA() noexcept;
 
-public:
-	std::string Encrypt(const std::string& password, const std::string& buffer) override;
-	std::string Decrypt(const std::string& password, const std::string& algotype_iv_cipher) override;
+  public:
+    std::vector<byte> Encrypt(const std::string& password, const std::vector<byte>& buffer) override;
+    std::vector<byte> Decrypt(const std::string& password, const std::vector<byte>& cipher) override;
+    void Encrypt(const std::string& password, const fs::path& in_filename, const fs::path& out_filename) override;
+    void Decrypt(const std::string& password, const fs::path& in_filename, const fs::path& out_filename) override;
 
-private:
-	std::unique_ptr<CryptoPP::EAX<CryptoPP::IDEA>::Encryption> m_idea_encryptor; /**< IDEA-EAX encryptor */
-	std::unique_ptr<CryptoPP::EAX<CryptoPP::IDEA>::Decryption> m_idea_decryptor; /**< IDEA-EAX decryptor */
+  private:
+    std::unique_ptr<CryptoPP::EAX<CryptoPP::IDEA>::Encryption> m_idea_encryptor; /**< IDEA-EAX encryptor */
+    std::unique_ptr<CryptoPP::EAX<CryptoPP::IDEA>::Decryption> m_idea_decryptor; /**< IDEA-EAX decryptor */
 };
 
 NS_ENIGMA_END
-#endif // !ENIGMA_IDEA_H
-
-
