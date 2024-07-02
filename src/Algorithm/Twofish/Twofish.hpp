52007c0d146164dd83b55a1d77bb23aec1d27484
Bader <badereddineouaich@gmail.com>
2024-07-02 17:46:26 +0100

    Handled large file support in Twofish algorithm

--- src/Algorithm/Twofish/Twofish.hpp
+++ src/Algorithm/Twofish/Twofish.hpp
@@ -1,31 +1,27 @@
 #pragma once
-#ifndef ENIGMA_TWOFISH_H
-#define ENIGMA_TWOFISH_H
-
 #include <Algorithm/Algorithm.hpp>
 
 NS_ENIGMA_BEGIN
 /**
 *	Twofish-GCM Algorithm Encryptor/Decryptor
 */
-class Twofish : public Algorithm
-{
-public:
-	/**
-	*	@param intent: Operation, Encrypt or Decrypt
-	*/
-	explicit Twofish(const Algorithm::Intent intent) noexcept;
-	virtual ~Twofish() noexcept;
+class Twofish : public Algorithm {
+  public:
+    /**
+    *	@param intent: Operation, Encrypt or Decrypt
+    */
+    explicit Twofish(const Algorithm::Intent intent) noexcept;
+    virtual ~Twofish() noexcept;
 
-public:
-	std::string Encrypt(const std::string& password, const std::string& buffer) override;
-	std::string Decrypt(const std::string& password, const std::string& algotype_iv_cipher) override;
+  public:
+    std::vector<byte> Encrypt(const std::string& password, const std::vector<byte>& buffer) override;
+    std::vector<byte> Decrypt(const std::string& password, const std::vector<byte>& cipher) override;
+    void Encrypt(const std::string& password, const fs::path& in_filename, const fs::path& out_filename) override;
+    void Decrypt(const std::string& password, const fs::path& in_filename, const fs::path& out_filename) override;
 
-private:
-	std::unique_ptr<CryptoPP::GCM<CryptoPP::Twofish>::Encryption> m_twofish_encryptor; /**< Twofish-GCM encryptor */
-	std::unique_ptr<CryptoPP::GCM<CryptoPP::Twofish>::Decryption> m_twofish_decryptor; /**< Twofish-GCM decryptor */
+  private:
+    std::unique_ptr<CryptoPP::GCM<CryptoPP::Twofish>::Encryption> m_twofish_encryptor; /**< Twofish-GCM encryptor */
+    std::unique_ptr<CryptoPP::GCM<CryptoPP::Twofish>::Decryption> m_twofish_decryptor; /**< Twofish-GCM decryptor */
 };
 
 NS_ENIGMA_END
-#endif // !ENIGMA_TWOFISH_H
-
