ffd57527d9d0e4f10c02a9c74a8702b4892f564a
Bader <badereddineouaich@gmail.com>
2024-07-04 21:31:07 +0100

    Updated IDEA Algorithm

--- src/Algorithm/IDEA/IDEA.hpp
+++ src/Algorithm/IDEA/IDEA.hpp
@@ -14,10 +14,12 @@ class IDEA : public Algorithm {
     *	@param intent: Operation, Encrypt or Decrypt
     */
     explicit IDEA(const Algorithm::Intent intent) noexcept;
-    virtual ~IDEA() noexcept;
+    ~IDEA() noexcept override;
 
   public:
+    std::vector<byte> Encrypt(const std::string& password, const byte* buffer, const std::size_t buffSize) override;
     std::vector<byte> Encrypt(const std::string& password, const std::vector<byte>& buffer) override;
+    std::vector<byte> Decrypt(const std::string& password, const byte* cipher, const std::size_t cipherSize) override;
     std::vector<byte> Decrypt(const std::string& password, const std::vector<byte>& cipher) override;
     void Encrypt(const std::string& password, const fs::path& in_filename, const fs::path& out_filename) override;
     void Decrypt(const std::string& password, const fs::path& in_filename, const fs::path& out_filename) override;
