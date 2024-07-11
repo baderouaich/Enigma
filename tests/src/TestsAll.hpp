54c1197f078e28e0270acd152c2a80c4609bcb49
Bader Ouaich <ouaichbadereddine@gmail.com>
2024-07-11 12:28:48 +0100

    Source headers group for Visual Studio IDE

--- tests/src/TestsAll.hpp
+++ tests/src/TestsAll.hpp
@@ -3,24 +3,24 @@
 #define ENIGMA_TESTS_ALL
 
 // Encryption Algorithms Tests
-//#include "Crypto/AESTests.hpp"
-//#include "Crypto/ChaCha20Poly1305Tests.hpp"
-//#include "Crypto/TripleDESTests.hpp"
-//#include "Crypto/TwofishTests.hpp"
-//#include "Crypto/IDEATests.hpp"
-//#include "Crypto/BlowfishTests.hpp"
+#include "Crypto/AESTests.hpp"
+#include "Crypto/ChaCha20Poly1305Tests.hpp"
+#include "Crypto/TripleDESTests.hpp"
+#include "Crypto/TwofishTests.hpp"
+#include "Crypto/IDEATests.hpp"
+#include "Crypto/BlowfishTests.hpp"
 //#include "Crypto/RSATests.hpp"
 
 // Utility Tests
-//#include "Utils/Base64Tests.hpp"
-//#include "Utils/FileUtilsTests.hpp"
+#include "Utils/Base64Tests.hpp"
+#include "Utils/FileUtilsTests.hpp"
 #include "Utils/HashTests.hpp"
 
 // Database Tests
 //#include "DB/CRUD.hpp"
 
 // Endianness
-//#include "Endianness/EndiannessTests.hpp"
+#include "Endianness/EndiannessTests.hpp"
 
 
 #endif // !ENIGMA_TESTS_ALL
