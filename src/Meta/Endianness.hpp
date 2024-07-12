3c97a7bffb05c9feb3110acd00e24449fe84d8a9
Bader <badereddineouaich@gmail.com>
2024-07-12 13:05:16 +0100

    __has_builtin portability

--- src/Meta/Endianness.hpp
+++ src/Meta/Endianness.hpp
@@ -7,10 +7,16 @@
 #include <cstdint>
 #include <bit>
 
+#ifdef __has_builtin
+#define ENIGMA_HAVE_BUILTIN(x) __has_builtin(x)
+#else
+#define ENIGMA_HAVE_BUILTIN(x) false
+#endif
+
 namespace Enigma {
 
   inline std::uint64_t BSwap64(std::uint64_t host_int) {
-#if defined(__GNUC__) || __has_builtin(__builtin_bswap64)
+#if defined(__GNUC__) || ENIGMA_HAVE_BUILTIN(__builtin_bswap64)
     return __builtin_bswap64(host_int);
 #elif defined(_MSC_VER)
     return _byteswap_uint64(host_int);
@@ -27,7 +33,7 @@ namespace Enigma {
   }
 
   inline std::uint32_t BSwap32(std::uint32_t host_int) {
-#if defined(__GNUC__) || __has_builtin(__builtin_bswap32)
+#if defined(__GNUC__) || ENIGMA_HAVE_BUILTIN(__builtin_bswap32)
     return __builtin_bswap32(host_int);
 #elif defined(_MSC_VER)
     return _byteswap_ulong(host_int);
@@ -40,7 +46,7 @@ namespace Enigma {
   }
 
   inline std::uint16_t BSwap16(std::uint16_t host_int) {
-#if defined(__GNUC__) || __has_builtin(__builtin_bswap16)
+#if defined(__GNUC__) || ENIGMA_HAVE_BUILTIN(__builtin_bswap16)
     return __builtin_bswap16(host_int);
 #elif defined(_MSC_VER)
     return _byteswap_ushort(host_int);
