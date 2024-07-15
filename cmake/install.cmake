ea160e52fbae8b8296d5e572824119f3554564bd
Bader <badereddineouaich@gmail.com>
2024-07-15 12:59:22 +0100

    Updated install script Do not install Enigma.db database everytime to not overwrite user data

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -17,30 +17,46 @@
 include(GNUInstallDirs)
 
 # Determine the installation directories
-if(WIN32)
+if (WIN32)
   set(INSTALL_BASE_DIR "$ENV{ProgramFiles}/Enigma")
-else()
+else ()
   # Get the user's home directory
-  if(NOT DEFINED ENV{HOME})
+  if (NOT DEFINED ENV{HOME})
     message(FATAL_ERROR "The HOME environment variable is not defined.")
-  endif()
+  endif ()
   set(USER_DOT_LOCAL_DIR "$ENV{HOME}/.local")
   set(INSTALL_BASE_DIR "$ENV{HOME}/Enigma")
-endif()
+endif ()
 
 # Install the executable to /home/$(whoami)/Enigma/Enigma
 install(TARGETS Enigma RUNTIME DESTINATION ${INSTALL_BASE_DIR})
 
 # Install res files to /home/$(whoami)/Enigma/res
+#install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/res/
+#  DESTINATION ${INSTALL_BASE_DIR}/res
+#  FILES_MATCHING PATTERN "*")
+
+# Install other resource files
 install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/res/
   DESTINATION ${INSTALL_BASE_DIR}/res
-  FILES_MATCHING PATTERN "*")
+  FILES_MATCHING PATTERN "*"
+  PATTERN "Enigma.db" EXCLUDE)  # Exclude Enigma.db database from this install command,
+# we don't want to overwrite user's data on an update
+# Check if Enigma.db exists and install it if it doesn't
+install(CODE "
+  if(NOT EXISTS \"${INSTALL_BASE_DIR}/res/database/Enigma.db\")
+    message(STATUS \"Enigma.db is missing. Installing it from repository's resources...\")
+    file(COPY \"${CMAKE_CURRENT_SOURCE_DIR}/res/database/Enigma.db\" DESTINATION \"${INSTALL_BASE_DIR}/res/database\")
+  else()
+    message(STATUS \"Enigma.db already exists. Skipping.\")
+  endif()
+")
 
 # Install .desktop file to /home/$(whoami)/Enigma/Enigma.desktop
-if(UNIX AND NOT APPLE)
- # Create the .desktop file content
+if (UNIX AND NOT APPLE)
+  # Create the .desktop file content
   file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/Enigma.desktop"
-"[Desktop Entry]
+    "[Desktop Entry]
 Type=Application
 Name=${PROJECT_NAME}
 Version=${PROJECT_VERSION}
@@ -56,4 +72,4 @@ Terminal=false"
 
 elseif (WIN32)
   # TODO create a shortcut of the app in windows
-endif()
+endif ()
